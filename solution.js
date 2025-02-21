897c7f9c779835a4be8c17f0fac70a43319341e8
BugHunter-X <askzenai@gmail.com>
2025-02-21 17:26:04 +0100

    Add solution.js file

---
+++ solution.js
@@ -0,0 +1,8 @@
+```javascript
+// Solution: Use the !important flag or adjust the specificity of the hover style
+<div class="bg-blue-500 p-4 text-white">
+  <button class="bg-red-500 hover:bg-red-700 !important">
+    Click Me
+  </button>
+</div>
+```
